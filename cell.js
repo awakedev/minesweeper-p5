39419926d4488862e9d627f40c9f0c50beec8b83
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2018-12-31 19:24:38 +0000

    implementing random mines

---
+++ cell.js
@@ -0,0 +1,30 @@
+function Cell(x, y, w) {
+    this.bee = true;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+
+    if (random(1) < 0.5) {
+        this.bee = true;
+
+    } else {
+
+        this.bee = false;
+    }
+
+    this.revealed = true;
+
+
+}
+
+Cell.prototype.show = function () {
+
+    stroke(0);
+    noFill();
+    rect(this.x, this.y, this.w, this.w)
+    if (this.revealed) {
+        if (this.bee) {
+            ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,15 +1,40 @@
-function Cell (){
-    this.bee = true;
-    this.revealed = true;
+function make2DArray(cols, rows) {
+    var arr = new Array(cols);
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
 }
 
+
+
+var grid;
+var w = 20;
+
+var rows = 20;
+
+var cols = 20;
+
 function setup() {
-        createCanvas(windowWidth, windowHeight);
-        background(255, 0, 200);
-      }
+    createCanvas(201, 201);
+    cols = floor(width / w);
+    rows = floor(height / w);
+    grid = make2DArray(cols, rows);
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Cell(i * w, j * w, w);
+        }
+    }
+
+}
 
 
 function draw() {
-    background(0);
-
+    background(255);
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show();
         }
+    }
+
+}
